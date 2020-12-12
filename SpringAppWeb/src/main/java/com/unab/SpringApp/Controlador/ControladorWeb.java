ec6a21cb8303a35a4663ba3aa86346497f2a3805
stanleeeys <69662538+stanleeeys@users.noreply.github.com>
2020-12-12 15:51:03 -0600

    inicion controlador

--- SpringAppWeb/src/main/java/com/unab/SpringApp/Controlador/ControladorWeb.java
+++ SpringAppWeb/src/main/java/com/unab/SpringApp/Controlador/ControladorWeb.java
@@ -1,5 +1,26 @@
 package com.unab.SpringApp.Controlador;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.unab.SpringApp.DAO.CategoriaDAO;
+import com.unab.SpringApp.Entidades.Categoria;
+
+@Controller
 public class ControladorWeb {
 
+	@Autowired
+	private CategoriaDAO seDAO;
+	
+	@GetMapping("/")
+	public String Inicio(Model m) {
+		
+		var Mcategoria = seDAO.findAll();
+		
+		m.addAttribute("categoria",Mcategoria);
+		
+		return"index";
+	}
 }
